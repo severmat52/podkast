1bc61a876a98d4f5aecefa15c5cf69fe754b3ef4
Matthew Severson <matthew.dennis.severson@gmail.com>
2018-12-20 18:18:53 -0600

    enough for today

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,13 +1,13 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
-import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import { Search } from './components/Search';
 
 export default () => (
   <Layout>
-    <Route exact path='/' component={Home} />
+    <Route exact path='/' component={Search} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
   </Layout>
