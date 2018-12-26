27d4edd97eef4af532646c81d25c47dd3028561e
Matthew Severson <matthew.dennis.severson@gmail.com>
2018-12-26 23:40:31 +0000

    Update ClientApp/src/store/configureStore.js

--- ClientApp/src/store/configureStore.js
+++ ClientApp/src/store/configureStore.js
@@ -1,13 +1,15 @@
-﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import * as Counter from './Counter';
 import * as WeatherForecasts from './WeatherForecasts';
+import * as Search from './Search';
 
 export default function configureStore(history, initialState) {
   const reducers = {
     counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer
+    weatherForecasts: WeatherForecasts.reducer,
+    search: Search.reducer
   };
 
   const middleware = [
