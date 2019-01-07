54f9684e054a3b93e9d293af821bd3949843d9e5
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-01-07 23:35:50 +0000

    Update ClientApp/src/components/AudioPlayer/AudioPlayer.js

--- ClientApp/src/components/AudioPlayer/AudioPlayer.js
+++ ClientApp/src/components/AudioPlayer/AudioPlayer.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Glyphicon } from 'react-bootstrap';
 import './AudioPlayer.css';
 
-const AudioPlayer = () => (
-    <div className='audio-player'>
-    <figure>
+class AudioPlayer extends Component{
+    constructor(props){
+        super(props);
+        this.state = { collapsed: false }
+    }
+    render(){
+        return this.state.collapsed 
+            ? this.renderCollapsed()
+            : this.renderAudioPlayer();
+    }
 
-    </figure>
-    </div>
-);
+    renderAudioPlayer(){
+        return(
+            <div className='audio-player'>
+            <figure>
+            <button onClick={() => this.setState({collapsed: !this.state.collapsed})}>
+                <Glyphicon glyph='chevron-up' />
+            </button>
+            </figure>
+            </div>
+        );
+    }
+
+    renderCollapsed(){
+        return  (<button className='audio-player-button-collapsed' onClick={() => this.setState({collapsed: !this.state.collapsed})}>
+                <Glyphicon glyph='chevron-up' />
+            </button>);
+    }
+}
 
 
 export default AudioPlayer;
