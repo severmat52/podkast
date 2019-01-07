1a729aec0e2a389315297f7dfd076d597802d42e
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-01-06 18:36:21 -0600

    progress... Logan's being a pain in the ass...

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,12 +5,17 @@ import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 import Search  from './components/Search';
 import PodcastFeed from './components/PodcastFeed';
+import AudioPlayer from './components/AudioPlayer/AudioPlayer';
 
 export default () => (
-  <Layout>
+  <div id='app'>
+<Layout>
     <Route exact path='/' component={Search} />
     <Route exact path='/feed' component={PodcastFeed} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
   </Layout>
+  <AudioPlayer />
+  </div>
+  
 );
---
+++ ClientApp/src/components/AudioPlayer/AudioPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './AudioPlayer.css';
+
+const AudioPlayer = () => (
+    <div className='audio-player'>
+    <figure>
+
+    </figure>
+    </div>
+);
+
+
+export default AudioPlayer;
