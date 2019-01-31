7ed62abcae45bc4ff40e674aaf8737fb1941e975
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-01-30 21:37:28 -0600

    edits without testing

--- ClientApp/src/components/Core/AudioButtonSmall.js
+++ ClientApp/src/components/Core/AudioButtonSmall.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Glyphicon } from 'react-bootstrap';
 import './AudioButton.css';
 
-const AudioButton = ({onClick, glyphicon}) => (
-    <button onClick={() => onClick()} className='audioButton'>
+const AudioButtonSmall = ({onClick, glyphicon}) => (
+    <button onClick={() => onClick()} className='audioButtonSmall'>
         <Glyphicon glyph={glyphicon} />
     </button>);
 
-export default AudioButton;
+export default AudioButtonSmall;
