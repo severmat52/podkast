355b055680a6517c9b6fa254006604eecfb03457
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-03-10 19:59:56 -0500

    adding ItunesFeedItem to fill in later when I have time

---
+++ Service/iTunesFeedParsing/ItunesFeedItem.cs
@@ -0,0 +1,20 @@
+﻿
+namespace podkast.Service.iTunesFeedParsing
+{
+    public class ItunesFeedItem
+    {
+        /*
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * https://github.com/simplepie/simplepie-ng/wiki/Spec:-iTunes-Podcast-RSS
+         * 
+         * 
+         * 
+         */
+
+    }
+}
