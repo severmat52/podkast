fbd9ed0fe82ae8ff1efb4af9711db3bb9e5e15c4
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-05-20 20:39:20 -0500

    moved everything

---
+++ podkast.shared/Dto/Parser/ITunesFeedData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Podkast.Shared.Dto.Parser
+{
+    public sealed class ITunesFeedData
+    {
+        public string Author { get; set; }
+        public string Block { get; set; }
+        public IEnumerable<ITunesCategory> Categories { get; set; }
+        public string Image { get; set; }
+        public string Explicit { get; set; }
+        public string Complete { get; set; }
+        public string NewFeedUrl { get; set; }
+        public string Owner { get; set; }
+        public string Summary { get; set; }
+        public string Language { get; set; }
+    }
+}
