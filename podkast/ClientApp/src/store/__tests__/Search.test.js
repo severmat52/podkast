b5e203b5805216e55471f8719497205bd4159b64
Matthew Severson <matthew.dennis.severson@gmail.com>
2019-10-12 17:37:48 -0500

    last commit before updating to .NET core 3.0 with new react template

--- podkast/ClientApp/src/store/__tests__/Search.test.js
+++ podkast/ClientApp/src/store/__tests__/Search.test.js
@@ -1,38 +1,38 @@
-import { actionCreators, requestSearchPodcastsType, receiveSearchPodcastsType} from '../Search'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import fetchMock from 'fetch-mock'
+import { actionCreators, requestSearchPodcastsType, receiveSearchPodcastsType} from '../Search';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 
 
 describe('Search Actions', () => {
     afterEach(() => {
-        fetchMock.restore()
-    })
+        fetchMock.restore();
+    });
 
 
     it('request search podcasts returns expected actions', () => {
 
-        const searchString = 'test search'
-        const podcastListResult = 'Test result'
+        const searchString = 'test search';
+        const podcastListResult = 'Test result';
 
         fetchMock.getOnce(`api/Search/${searchString}`,{
             body: { podcasts: podcastListResult },
             headers: {'content-type': 'application/json'}
-        })
+        });
 
         const expectedActions = [
             { type: requestSearchPodcastsType, searchString },
             { type: receiveSearchPodcastsType, searchString, podcastListResult }
-        ]
+        ];
 
-        const store = mockStore({})
+        const store = mockStore({});
 
         return store.dispatch(actionCreators.requestSearchPodcasts(searchString)).then(() => {
-            expect(store.getActions()).toEqual(expectedActions)
-        })
-    })
-})
+            expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+});
